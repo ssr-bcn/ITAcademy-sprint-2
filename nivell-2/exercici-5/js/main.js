df923d9bf6de73bfaa290c08610f4a7d36b3ef28
SSR <ssr@estamosenello.net>
2022-03-21 00:32:55 +0100

    Bootstrap Icons canviats a Fontawesome

--- nivell-2/exercici-5/js/main.js
+++ nivell-2/exercici-5/js/main.js
@@ -9,10 +9,10 @@
   Array.prototype.slice.call(forms)
     .forEach(function (form) {
       form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
+//        if (!form.checkValidity()) {
           event.preventDefault()
           event.stopPropagation()
-        }
+//        }
 
         form.classList.add('was-validated')
       }, false)
